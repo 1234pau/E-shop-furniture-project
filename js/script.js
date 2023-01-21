b21c61ec0937aa9a2b6569db2a0b6b06dd00b06d
paul geana <87538456+1234pau@users.noreply.github.com>
2023-01-21 15:48:01 +0200

    add more hero images and animation

--- js/script.js
+++ js/script.js
@@ -1,35 +1,40 @@
 const arrowLeft = document.querySelector(".arrowLeft")
 const arrowRight = document.querySelector(".arrowRight")
+const buttonRight = document.querySelector(".right")
+const buttonleft = document.querySelector(".left")
 
 
-const images = ["pngwing.com.png","pngwing.com(1).png", "pngwing.com(2).png", "pngwing.com(3).png"]
+
+const images = ["pngwing.com.png","pngwing.com(1).png", "pngwing.com(2).png", "pngwing.com(3).png", "pngwing.com(4).png", "pngwing.com(5).png"]
 
 let nr = 0
 const lengthOfArray = images.length
+console.log(lengthOfArray)
 
 arrowLeft.addEventListener("click", ()=>{
-  if(nr == 1 || nr == 2 || nr == 3 || nr == 4){
-  const imageSrc = document.querySelector("#image").src= `assets/${images[nr--]}`
-  }else if(nr == 0){
-    const imageSrc = document.querySelector("#image").src= `assets/${images[0]}`
-    return
-  }else{
-    return
-  }
+  if(nr === 0){
+      return
+    }else{
+      buttonRight.style.fill = "#030303"
+      nr--
+    }
+    if(nr === 0){
+      buttonleft.style.fill = "grey"
+    }
+    const imageSrc = document.querySelector("#image").src= `assets/${images[nr]}`
   console.log(nr)
-  console.log("left")
 })
-console.log(lengthOfArray)
+
 arrowRight.addEventListener("click", ()=>{
-  
-  
   if(nr === lengthOfArray-1){
     return
   }else{
+    buttonleft.style.fill = "#030303"
     nr++
-
+  }
+  if(nr === lengthOfArray-1){
+    buttonRight.style.fill = "grey"
   }
   const imageSrc = document.querySelector("#image").src= `assets/${images[nr]}`
   console.log(nr)
-  console.log("right")
 })
