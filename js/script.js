61342371dcb607f7dde48147e0236893776c6df8
paul geana <87538456+1234pau@users.noreply.github.com>
2023-01-25 14:19:07 +0200

    .

--- js/script.js
+++ js/script.js
@@ -4,6 +4,7 @@ const buttonRight = document.querySelector(".right")
 const buttonleft = document.querySelector(".left")
 const containerItems = document.querySelector(".containerItems")
 const hide = document.querySelector(".hide")
+
 const images = ["pngwing.com.png", "pngwing.com(1).png", "pngwing.com(2).png", "pngwing.com(3).png", "pngwing.com(4).png", "pngwing.com(5).png"]
 
 let nr = 0
@@ -40,22 +41,19 @@ arrowRight.addEventListener("click", () => {
 /*****************CUSTOME CARD ELEMENT******************/
 
 const IMAGES = [{
-            imagePath: "pngwing.com.png",
-            nameOfImage: "Chair"
-        },
-        {
-            imagePath: "pngwing.com(1).png",
-            nameOfImage: "Sofa"
-        },
-        {
-            imagePath: "pngwing.com(2).png",
-            nameOfImage: "Armchair",
+        imagePath: "pngwing.com(5).png",
+        nameOfImage: "Sofa"
+    },
+    {
+        imagePath: "pngwing.com(6).png",
+        nameOfImage: "Chair"
+    },
+    {
+        imagePath: "pngwing.com(2).png",
+        nameOfImage: "Armchair",
 
-        },
-
-
-    ]
-    //IMAGES.length = 3
+    },
+]
 
 function createCard(images) {
 
@@ -89,62 +87,41 @@ function createCard(images) {
 }
 
 createCard(IMAGES)
-    //console.log("outside", IMAGES.length)
 let condition = true
 
 hide.addEventListener("click", () => {
-        containerItems.innerHTML = ""
-        setTimeout(() => {
-            window.location.hide(true);
-        }, 200);
-        //console.log("outside", condition)
-        if (condition === true) {
-            IMAGES.pop()
-            IMAGES.pop()
-            IMAGES.pop()
+    containerItems.innerHTML = ""
+    if (condition === true) {
+        IMAGES.splice(0, 3)
+        IMAGES.push({
+            imagePath: "pngwing.com(5).png",
+            nameOfImage: "Sofa"
+        }, {
+            imagePath: "pngwing.com(6).png",
+            nameOfImage: "Chair"
+        }, {
+            imagePath: "pngwing.com(2).png",
+            nameOfImage: "Armchair",
 
-            IMAGES.push({
-                    imagePath: "pngwing.com.png",
-                    nameOfImage: "Chair"
-                }, {
-                    imagePath: "pngwing.com(1).png",
-                    nameOfImage: "Sofa"
-                }, {
-                    imagePath: "pngwing.com(2).png",
-                    nameOfImage: "Armchair",
+        }, {
+            imagePath: "pngwing.com(9).png",
+            nameOfImage: "Beds",
 
-                }, {
-                    imagePath: "pngwing.com(3).png",
-                    nameOfImage: "Beed",
-
-                }, {
-                    imagePath: "pngwing.com(4).png",
-                    nameOfImage: "Coach"
-                }, {
-                    imagePath: "pngwing.com(5).png",
-                    nameOfImage: "Otoman"
-                }, )
-                // console.log("if statement", IMAGES.length)
-                // console.log("object", IMAGES)
-                // console.log("inside", condition)
-            createCard(IMAGES)
-            condition = false
-        } else if (condition == false) {
-            IMAGES.pop()
-            IMAGES.pop()
-            IMAGES.pop()
-            createCard(IMAGES)
-                //console.log("else if statement", IMAGES.length)
-            condition = true
-        }
-
-    })
-    // window.addEventListener("load", (event) => {
-    //     console.log("component is fully loaded");
-    // });
-    // document.addEventListener("DOMContentLoaded", (event) => {
-    //     console.log("component is fully loaded");
-    // });
-    // document.addEventListener("readystatechange", (event) => {
-    //     console.log("component is fully loaded");
-    // });
+        }, {
+            imagePath: "pngwing.com(10).png",
+            nameOfImage: "Tables"
+        }, {
+            imagePath: "pngwing.com (11).png",
+            nameOfImage: "Wardrobes"
+        }, )
+        createCard(IMAGES)
+        condition = false
+    } else if (condition == false) {
+        IMAGES.splice(-3)
+        createCard(IMAGES)
+        condition = true
+    }
+})
+setTimeout(() => {
+    window.location.hide(true);
+}, 200);
